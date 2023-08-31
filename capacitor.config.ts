5cd244a5dc1f8f7646cc4431ef855dadddbb4a83
aaronduacode <aaron.martinez@duacode.com>
2023-08-31 13:06:02 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'prueba-uve',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
